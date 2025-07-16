2252e049b34c04c9124798d8dd1e879c341a3c4d
Roxana del Toro <rxdeltoro@gmail.com>
2025-07-16 12:52:28 -0700

    Adds Node js and gets it working

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"[Insert your name here]" });
+  res.render('index', { title: 'CSC 317 App', name:"Roxana del Toro" });
 });
 
 module.exports = router;
+
+var express = require('express')
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+  res.send('Hello World!');
+})
+
+router.get('/login', function (req, res, next) {
+  res.render('index', {title: 'CSC 317 App'});
+})
+
+router.get('/register', function (req, res, next) {
+  res.render('register', {title: 'Register'});
+})
+
+router.get('/post', function (req, res, next) {
+  res.render('post', {title: 'Post video'});
+})
+
+router.get('/viewpost', function (req, res, next) {
+  res.render('vewe', {title: 'View post'});
+})
